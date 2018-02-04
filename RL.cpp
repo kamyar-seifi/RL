b57cd11ac183e103cb52a91e12da17c966ab9725
Kamyar Seifi <32001776+kseifi@users.noreply.github.com>
2018-02-03 16:09:09 -0800

    init RL

---
+++ RL.cpp
@@ -0,0 +1,145 @@
+//
+//  RL.cpp
+//  
+//
+//  Created by kamyar Seifi.
+//
+//
+
+#include "RL.h"
+#include "math.h"
+#include <iostream>
+Encode::RL::RL(vector<unsigned char>& input):dta(input){
+
+}
+
+int Encode::RL::Compress(){
+    
+   // vector<pair<unsigned char, unsigned char>> no_rep_seq;  //vector for repeated element count <element, count>
+    
+    auto no_rep_seq = ReturnMap();
+    
+    if(no_rep_seq->empty())
+        return -2;
+    
+    int new_size = ceil(no_rep_seq->size()*1.5 + 0.5) ; //size approximation: rounded version of this number. second count if not existant will be zero.
+    
+    if(new_size>=dta.size())  //encode the input if file can be cocmpressed.
+        return -1;
+    else{
+        dta.clear(); //clearing the input data to write encoded file.
+        for(auto i : (*no_rep_seq))
+            dta.push_back(i.first);  //write the non-repeated data from the map
+        
+        //write a flag and the first count in the initial header byte.
+        auto temp_first = no_rep_seq->front().second;
+        temp_first = 0x80 | temp_first;
+        dta.push_back((unsigned char)temp_first);
+        no_rep_seq->erase(no_rep_seq->begin());
+        
+        
+        //write the rest of the count header from the map vector
+        while(!no_rep_seq->empty()){
+            auto temp = no_rep_seq->front().second;
+            no_rep_seq->erase(no_rep_seq->begin());
+            temp = temp << 4;
+            if(!no_rep_seq->empty()){
+                temp = temp | (no_rep_seq->front().second);
+                no_rep_seq->erase(no_rep_seq->begin());
+            }
+            dta.push_back(temp);
+        }
+    }
+    delete no_rep_seq;
+    return (int)dta.size();  //the size of encoded input.
+    }
+
+
+bool Encode::RL::IsCompressed(){
+    int header=0;
+    for(;( (dta[header] >> 4 ) < 8) && header<dta.size(); header++);
+    if(header==dta.size())
+        return false;
+    else
+        return true;
+}
+
+auto Encode::RL::ReturnMap(void)->vector<pair<unsigned char, unsigned char>>*{
+    
+    auto map = new vector<pair<unsigned char, unsigned char>>;
+    (*map).clear();
+    
+    if(!dta.empty()){
+        for(int i=0;i<(int)dta.size() ; i++){   //loop through the input data
+            int j=i;
+            int count=1;
+            for(;dta[j]==dta[j+1]; j++);  //find the repeated elements
+        
+            count += j-i;  //calculate the repetition count;
+            i=j; //jump to nonrepeated element next round
+        
+            if(count<=15){
+                pair<unsigned char, unsigned char> temp = {dta[j], count}; //save the <element, count> in a pair
+                (*map).push_back(temp);  // add these pairs to the map vector
+            }
+            else
+                (*map).clear();
+        }
+    }
+        return map;
+}
+
+
+int Encode::RL::Decompress(){
+    int header=0;
+
+    if(!IsCompressed())
+        return -1;
+    else
+        for(;( (dta[header] >> 4 ) < 8) && header<dta.size(); header++);
+    
+    int read_header=1;  //helper flag for reading the count bytes;
+    vector<pair<unsigned char, unsigned char>> map;
+    
+    for(int i=0; (dta[i] >> 4) < 0x08; i++)
+    {
+        unsigned char count = dta[header];
+        if(read_header==0){  //reaading the second digit of the count byte
+            count = count & 0xf0;
+            count = count >> 4;
+            read_header=1;
+        }else{  //reading the frist digit of the count byte
+            count = count & 0x0f;
+            read_header=0;
+            header++;
+        }
+        pair<unsigned char, unsigned char> temp = {dta[i], count};
+        map.push_back(temp);
+    }
+    
+    dta.clear(); //clear the encoded input
+    
+    while(!map.empty()){     //decode the <element, count> vector into the input vector
+        auto a = map.front();
+        for(unsigned char i=0; i<a.second; i++)
+            dta.push_back(a.first);
+        map.erase(map.begin());
+    }
+    return (int) dta.size();
+}
+
+
+void Encode::RL::Disp(){
+    cout << endl;
+if(dta.empty())
+    cout << "Error No data found" << endl;
+    else if(IsCompressed()){
+        cout << "Stat: RL-Compressed" << endl;
+        for(auto i: dta)
+            printf(" %x ", i);
+    }else{
+        cout << "Stat: Not-Compressed " << endl;
+        for(auto i: dta)
+            printf(" %x ", i);
+    }
+}
---
+++ RL.h
@@ -0,0 +1,42 @@
+//
+//  RL.h
+//  working
+//
+//  Created by kamyar Seifi on 2018-02-03.
+//  Copyright © 2018 kamyar Seifi. All rights reserved.
+//
+
+#ifndef RL_h
+#define RL_h
+#include <vector>
+using namespace std;
+
+namespace Encode { class RL{
+    
+private:
+    vector<unsigned char> dta;
+    RL();
+    
+public:
+    //@RL constructor: data loaded and initialized
+    //@input: the first argument loading the input into the dta;
+    RL(vector<unsigned char>& input);
+    
+    //@Compressed: the Run-lenght compressor
+    //@return: return the size of the data
+    int Compress(void);
+    
+    //@Iscompressed: predicator for checking the compressed data
+    bool IsCompressed(void);
+    //@Decompress: decompressing the data
+    int Decompress(void);
+    
+    void Disp(void); 
+    
+    //@ReturnMap: the map of the pair of the count and element
+    //@return: allocate the map dynamically, return a pointer to it. user is responsible to free the allocated map
+    auto ReturnMap(void)->vector<pair<unsigned char, unsigned char>>*;
+};
+};
+
+#endif /* RL_h */
